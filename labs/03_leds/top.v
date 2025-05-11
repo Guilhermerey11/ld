be9fa117cfbf454898f2f8c301d364c4957c3761
Guilhermerey11 <guilhermealves@estudante.ufscar.br>
2025-05-11 14:57:21 -0300

    Update top.v

--- labs/03_leds/top.v
+++ labs/03_leds/top.v
@@ -4,4 +4,9 @@ module top(
 
 	assign led = sw;
 	//assign led[0] = ~sw[0];
+	assign led[0] = ~sw[0];  // Inverte o valor da chave 0 para o LED 0
+	assign led[1] = sw[1];  
+	assign led[2] = sw[2];  
+	assign led[3] = sw[3]; 
+
 endmodule
