23f08fe5c04508ab30dda3b8d10e651a70ad96d8
Guilhermerey11 <guilhermealves@estudante.ufscar.br>
2025-07-25 00:18:16 -0300

    Update topA.v

--- labs/04_mux3/topA.v
+++ labs/04_mux3/topA.v
@@ -1,28 +1,35 @@
 module top (
     input x1, x2, x3, s0, s1,
-    output xfe, xff);
-    mux3e dute(x1, x2, x3, s0, s1, xfe);
-    mux3f dutf(x1, x2, x3, s0, s1, xff);
+    output xfe, xff
+);
+    mux3e dute(x1, x2, x3, s0, s1, xfe); // estrutural
+    mux3f dutf(x1, x2, x3, s0, s1, xff); // funcional
 endmodule 
 
 // Estrutural
 module mux3e (
     input x1, x2, x3, s0, s1,
-    output f);
-    // Instancie e conecte os componentes abaixo  
-
+    output f
+);
+    // Instancia e conecta os componentes abaixo
+    wire w1;
+    mux2 m1(w1, s0, x1, x2); // Seleciona entre x1 e x2 com s0
+    mux2 m2(f,  s1, w1, x3); // Seleciona entre w1 e x3 com s1
 endmodule
 
 // Funcional
 module mux3f (
     input x1, x2, x3, s0, s1,
-    output f);
-    // Digite o seu código abaixo  
-
+    output f
+);
+    // Implementação condicional direta
+    assign f = (s1 == 0) ? (s0 ? x2 : x1) : x3;
 endmodule
 
-module mux2(
+// MUX 2:1 genérico
+module mux2 (
     output f,
-    input s, x1, x2);
+    input s, x1, x2
+);
     assign f = s ? x2 : x1;
 endmodule
